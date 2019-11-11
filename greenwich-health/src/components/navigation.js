c477b0d180729fc8c7d33ad54a5814a50edf7027
Steve Lopez <Bygonaut@yahoo.com>
2019-11-11 14:38:24 -0500

    Update navigation.js

--- greenwich-health/src/components/navigation.js
+++ greenwich-health/src/components/navigation.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 const sections = [
-    "Informed Consent For Clinical Research",
-    "Why is this study being done?",
-    "Is there a potential conlfict of interest for thids study?",
-    "How was I selected to be in this study?"
+    {id:0, title:"Informed Consent For Clinical Research"},
+    {id:1, title:"Why is this study being done?"},
+    {id: 2, title:"Is there a potential conlfict of interest for thids study?"},
+    {id:3, title:"How was I selected to be in this study?"}
 ];
 
 export default function Navigation() {
     return (
         <ul>
-        {sections.map(section => 
-            <li>{section}</li>
-        )}     
+        {sections.map(section=> (
+            <li><a href="#" >{section.title}</a></li>
+        ))}
+        
         </ul>    
     );
    
