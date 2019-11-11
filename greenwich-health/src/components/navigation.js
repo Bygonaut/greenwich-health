9629ec23ae404c7085a1e2ebb1ca2458cb453909
Steve Lopez <Bygonaut@yahoo.com>
2019-11-11 16:59:37 -0500

    added email functionality, study number, video and image

--- greenwich-health/src/components/navigation.js
+++ greenwich-health/src/components/navigation.js
@@ -5,7 +5,8 @@ const sections = [
     {id:0, title:"Considerations for Participation"},
     {id:1, title:"Why is this study being done?"},
     {id: 2, title:"Who Conducts Clinical Studies?"},
-    {id:3, title:"How Are Participants Protected?"}
+    {id:3, title:"How Are Participants Protected?"},
+    {id:4, title:"Foods to Prevent Brain Aging"}
 ];
 
 export default function Navigation() {
@@ -18,6 +19,8 @@ export default function Navigation() {
     return (
         <React.Fragment>
             <div className="flex-initial bg-gray-100 py-10 px-2 w-56">
+                <p className="text-2xl">Study 20-121</p>
+                <hr></hr><br/>
                 <ul>
                 {sections.map(section => 
                     <li className="mb-5"><button value={section.id} onClick={e => changeInputValue(e.target.value)} className="w-full text-left">{section.title}</button></li>
--- greenwich-health/src/components/question-button.js
+++ greenwich-health/src/components/question-button.js
@@ -1,18 +1,46 @@
 import React, {useState} from "react";
 
+
 export default function QuestionButton(props) {
     const [showTextBox, setTextBox] = useState(
         false
         );
+    const [feedback, setFeedback] = useState("");
+    const name = 'Study Participant 1', email = 'devhack69@gmail.com';
+    function handleChange(event) {
+        this.setState({feedback: event.target.value})
+      }
+    
+     function handleSubmit (event) {
+        const templateId = 'template_tl1QGIRb';
+    
+        sendFeedback(templateId, {message_html: feedback, from_name: name, reply_to: email});
+      }
+    
+     function sendFeedback (templateId, variables) {
+        window.emailjs.send(
+          'gmail', templateId,
+          variables
+          ).then(res => {
+            console.log('Email successfully sent!')
+          })
+          // Handle errors here however you like, or use a React error boundary
+          .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+      }
+
     return (
     <div className="fixed inline right-0 bottom-0 mb-6 mr-6">
         <div className={`bg-teal-500 w-12 h-12 flex justify-center items-center rounded-full ${showTextBox ? 'hidden' : 'block'}`}>
             <button onClick={() => setTextBox(true)}><svg style={{fill:'white'}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"/><path d="M0 0h24v24H0z" fill="none"/></svg></button>
         </div>
         <div className={`w-64 h-64 bg-white border-gray-600 border rounded flex flex-col ${showTextBox ? 'block' : 'hidden'}`}>
-            <textarea className="flex-1 p-2" style={{resize:'none'}}></textarea>
-            <button onClick={() => setTextBox(false)} className="bg-teal-500 w-full">Send</button>
+            <textarea className="flex-1 p-2" 
+            onChange={e => setFeedback(e.target.value)} 
+            placeholder="Post some lorem ipsum here" 
+            style={{resize:'none'}}></textarea>
+            <button onClick={() => {setTextBox(false); handleSubmit();}} className="bg-teal-500 w-full">Send</button>
         </div>
     </div>
     )
 }
+
