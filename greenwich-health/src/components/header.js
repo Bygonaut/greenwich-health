fa1430afcd7e6c4e843647257cbdd7a03d1cb701
Josh Levine <josh@Joshs-MacBook-Pro.local>
2019-11-11 16:22:13 -0500

    added messaging

--- greenwich-health/src/components/header.js
+++ greenwich-health/src/components/header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Header() {
     return (
-        <div className="h-20 bg-teal-500 text-white flex justify-center items-center text-xl font-bold">
-            <span>Unnamed eConsenting Platform</span>
+        <div className="h-20 bg-teal-500 text-white flex justify-center items-center text-2xl font-bold">
+            <span>Journey</span>
         </div>  
     );
    
--- greenwich-health/src/components/question-button.js
+++ greenwich-health/src/components/question-button.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function QuestionButton(props) {
-    return (<div className="fixed inline right-0 bottom-0 bg-white mb-6 mr-6">
-        <button>Questions?</button>
-    </div>)
+    const [showTextBox, setTextBox] = useState(
+        false
+        );
+    return (
+    <div className="fixed inline right-0 bottom-0 mb-6 mr-6">
+        <div className={`bg-teal-500 w-12 h-12 flex justify-center items-center rounded-full ${showTextBox ? 'hidden' : 'block'}`}>
+            <button onClick={() => setTextBox(true)}><svg style={{fill:'white'}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"/><path d="M0 0h24v24H0z" fill="none"/></svg></button>
+        </div>
+        <div className={`w-64 h-64 bg-white border-gray-600 border rounded flex flex-col ${showTextBox ? 'block' : 'hidden'}`}>
+            <textarea className="flex-1 p-2" style={{resize:'none'}}></textarea>
+            <button onClick={() => setTextBox(false)} className="bg-teal-500 w-full">Send</button>
+        </div>
+    </div>
+    )
 }
