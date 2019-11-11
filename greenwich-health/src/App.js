2db36282da8354eace441c442336fefb462a4a1c
Steve Lopez <Bygonaut@yahoo.com>
2019-11-11 15:24:55 -0500

    react hooks to update state of current component

--- greenwich-health/src/App.js
+++ greenwich-health/src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import Navigation from './components/navigation';
 import Study from './components/study';
 import Header from './components/header';
 
+export const AppContext = React.createContext();
+
+// Set up Initial State
+const initialState = {
+  sectionId: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+      case 'UPDATE_SECTION':
+          return {
+            sectionId: action.data
+          };
+
+
+      default:
+          return initialState;
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="flex flex-col w-screen h-screen">
       <Header/>
     <div className="flex w-full h-full">
-      <Navigation/>
-      <Study />
+      <AppContext.Provider value={{ state, dispatch }}>
+        <Navigation/>
+        <Study />
+      </AppContext.Provider>
     </div>
     </div>
   );
 }
-
 export default App;
